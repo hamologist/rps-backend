ccd1c6230d38fdc7be2c05035b441db1e214d264
Jordan Paris <hamologist@gmail.com>
2025-02-15 21:06:26 -0500

    Demo implementation of a websocket server in Bun

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("Hello via Bun!");
+import { z } from 'zod';
+
+const env = z.object({
+  HOST: z.string(),
+  PORT: z.coerce.number(),
+}).parse(process.env);
+
+type WebSocketData = {
+  connectionId: string;
+};
+
+const server = Bun.serve<WebSocketData>({
+  fetch(req, server) {
+    const url = new URL(req.url);
+    if (url.pathname === "/websocket") {
+      const upgraded = server.upgrade(req, {
+        data: {
+          connectionId: Bun.randomUUIDv7(),
+        },
+      });
+      if (!upgraded) {
+        return new Response("Upgrade failed", { status: 400 });
+      }
+    }
+    return new Response("Hello World");
+  },
+  websocket: {
+    async message(ws, message) {
+      ws.send(`Your connectionId is: ${ws.data.connectionId}`);
+    },
+  },
+  port: env.PORT,
+  hostname: env.HOST,
+});
+
+console.debug(`Listening on ${server.hostname}:${server.port}`);
